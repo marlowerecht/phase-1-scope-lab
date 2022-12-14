e66a4ce4ce0c46ba86bdfced8c3d7f9297a1786c
Marlowe Recht <recht.marlowe@gmail.com>
2022-12-14 01:34:33 +0100

    commit tree c55935fd99d0e649348f50371f2fd5b036a9016e

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+    return customerName = customerName.toUpperCase();
+}
+
+upperCaseCustomerName();
+
+function setBestCustomer() {
+    var bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer() {
+    var bestCustomer = "maybe bob";
+}
+
+const leastFavoriteCustomer = "no one";
+
+function changeLeastFavoriteCustomer() {
+    const leastFavoriteCustomer = "nobody";
+}
